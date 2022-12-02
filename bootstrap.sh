4659ccea19d5df0ddaad28a8e8e5bb8ff2ba47f0
David Beitey <david@davidjb.com>
2022-12-02 11:47:48 +1000

    Increase default file resource limits

--- bootstrap.sh
+++ bootstrap.sh
@@ -619,6 +619,11 @@ configure_mac () {
     echo "$(which bash)" | sudo tee -a /etc/shells
     chsh -s "$(which bash)"
 
+    # Increase file resource limits
+    echo 'kern.maxfiles=20480' | sudo tee -a /etc/sysctl.conf
+    echo -e 'limit maxfiles 8192 20480\nlimit maxproc 1000 2000' | sudo tee -a /etc/launchd.conf
+    echo 'ulimit -n 4096' | sudo tee -a /etc/profile
+
     # Remap keyboard keys - keyboard brightness up/down
     cp etc/com.local.KeyRemapping.plist /Library/LaunchAgents/com.local.KeyRemapping.plist
     echo 'Keyboard remapping installed. Ensure you reboot for this to take effect.'
